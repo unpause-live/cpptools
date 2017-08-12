d355d252d17e0bde8bcf18c40453c43f17d48cb3
James Hurley <jamesghurley@gmail.com>
2017-08-12 08:30:50 -0400

    Update tests

--- include/unpause/__unpause/async/run_loop.hpp
+++ include/unpause/__unpause/async/run_loop.hpp
@@ -1,3 +1,12 @@
+/* Copyright (c) 2017 Unpause, Inc.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *  See the file LICENSE included with this distribution for more
+ *  information.
+ */
 #ifndef UNPAUSE_ASYNC_RUN_LOOP_HPP
 #define UNPAUSE_ASYNC_RUN_LOOP_HPP
 
