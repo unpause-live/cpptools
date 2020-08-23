69ae8532e9aa902b73b7b8471841948addcee8ea
James Hurley <jamesghurley@gmail.com>
2020-08-23 11:27:14 +0200

    Update copyright

--- include/unpause/__unpause/log.h
+++ include/unpause/__unpause/log.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017 Unpause, Inc.
+/* Copyright (c) 2020 Unpause, SAS.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
